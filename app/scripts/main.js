a14d75728461d10ca95387a41c178204dff429ad
Saun Whittenburg <saunwhittenburg@Sauns-MacBook-Pro.local>
2014-06-17 18:38:11 -0400

    resized boxes and worked on placement

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,7 @@
 etsyitems.forEach(function(a){
 	a.results.forEach(function(b){
-		$('.main-body').append('<div class="item"> <div class="name">' + b.title + 
-			'</div>' + '<div class="price">' + '$' + b.price + '</div> <div class=currency_code>'
-			 + b.currency_code +'</div>' + '<div class="images"><img src="' + b.Images[0].url_170x135 + 
-			 '"/></div>' + '<div class="user-id">'+ b.user_id + '</div></div>	');
+		$('.main-body').append('<div class="item"> <div class="images"><img src="' + b.Images[0].url_170x135 + 
+			 '"/></div> <div class="name">' + b.title + '</div>' + '<div class="price">' + '$' + b.price + '</div> <div class=currency_code>'
+			 + b.currency_code +'</div>'  + '<div class="user-id">'+ b.user_id + '</div></div>	');
 	});
 });
