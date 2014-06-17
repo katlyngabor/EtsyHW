e2bc8521c6c66a04a62c718ef3645b11f66218dd
Saun Whittenburg <saunwhittenburg@Sauns-MacBook-Pro.local>
2014-06-17 18:03:15 -0400

    resized images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
 etsyitems.forEach(function(a){
 	a.results.forEach(function(b){
-		$('.main-body').append('<div class="name">' + b.title + '</div>' + '<div class="price">' + '$' + b.price + 
-			'</div>' + '<div class="images"><img src=' + b.Images[0].url_75x75 + '/></div>');
+		$('.main-body').append('<div class="item"> <div class="name">' + b.title + 
+			'</div>' + '<div class="price">' + '$' + b.price + '</div> <div class=currency_code>'
+			 + b.currency_code +'</div>' + '<div class="images"><img src="' + b.Images[0].url_170x135 + 
+			 '"/></div>' + '<div class="user-id">'+ b.user_id + '</div></div>	');
 	});
 });
-
